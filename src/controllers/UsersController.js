d1c21d7df9948110f99ebb8e155e1a83eb88f45b
Davi Hillesheim <hillesheimdavi@gmail.com>
2021-05-12 12:16:34 -0300

    create the UserController module to abstract user creation on routes.js

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,5 +1,53 @@
+const knex = require('../database/connection');
+const bcrypt = require('bcryptjs');
+
 module.exports = {
-  // all the knex use should be made in here;
-  // separate bcrypt usage to an utils.js file and import it here, to 
-  // take the hash of the password and store it in the db
+
+  async signUp(req, res) {
+    const {email, name, password } = req.body;
+    let passwordHash;
+  
+    bcrypt.hash(password, 10, (err, hash) => {
+      if (err) {
+          console.log(err);
+      } else {
+        passwordHash = hash;
+      }
+      
+    });
+
+    const trx = await knex.transaction();
+
+    const user = {
+      email,
+      name,
+      password: passwordHash
+    }
+
+    await trx('users').insert(user);
+    await trx.commit();
+
+    return res.json({
+      email,
+      name,
+      passwordHash
+    })
+  },
+
+  async signIn(req, res) {
+    const {email, password} = req.body;
+
+    const user = await knex.select('account_id', 'email', 'password').from('users').where('email', '=', email);
+    console.log(user)
+
+    if(!user) {
+      return res.status(400).send({ error: 'User not found'});
+    }
+
+    if(!await bcrypt.compare(password, user[0].password)) {
+      return res.status(400).send({ error: 'Invalid email or password'});
+    }
+
+    res.send({user})
+  }
 }
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const routes = express.Router();
+routes.use(express.json());
+const knex = require('./database/connection');
 
 const CategoryController = require('./controllers/CategoryController');
 const TransactionController = require('./controllers/TransactionController');
@@ -9,46 +11,8 @@ routes.get('/categories', (req, res)=> {
   knex('categories').then((results) => res.json(results))
 });
 
-routes.post('/signin', (req, res) => {
-  const {email, password} = req.body;
+routes.post('/signin', UserController.signIn);
 
-  console.log(email)
-
-  knex.select('id', 'email', 'password').from('users')
-      .where('email', '=', req.body.email)
-      .then(user => {
-          bcrypt.compare(password, user[0].password, (error, response) => {
-              if(response) {
-                  res.send(user[0]);
-              } else {
-                  res.send({message: 'Wrong password and/or username.'});
-              }
-          })
-      });
-});
-
-routes.post('/register', (req, res) => {
-  const {email, name, password } = req.body;
-  
-  bcrypt.hash(password, 10, (err, hash) => {
-      if (err) {
-          console.log(err);
-      }
-
-      knex('users')
-      .returning('*')
-      .insert({
-          email: email,
-          name: name,
-          password: hash
-      })
-      .then(user => {
-          res.json(user);
-      })
-      .catch(err => {
-          res.status(400).json('Unable to register');
-      })
-  });
-});
+routes.post('/register', UserController.signUp);
 
 module.exports = routes;
