707e0d6708ef4b1d5ec782254f393c47684a5c93
Davi Hillesheim <hillesheimdavi@gmail.com>
2021-05-19 16:14:09 -0300

    little bug corrections

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
  
@@ -7,6 +8,8 @@ const app = express();
 app.use(routes);
 app.use(express.json());
 app.use(cors());
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3001, () => {
     console.log('App is running on port 3001');
